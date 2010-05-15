46ebc21108bd20c653e9a94479a844339453123b
Neonkoala <neonkoala@nkmini-ubuntu.(none)>
2010-05-15 12:57:48 +0100

    Changed version from 0.1 to 0.1.1 to more easily detect temp-os support.

--- openiboot/commands.c
+++ openiboot/commands.c
@@ -412,7 +412,7 @@ void cmd_install(int argc, char** argv) {
 	bufferPrintf("Installing Images...\r\n");
 	images_install(&_start, (uint32_t)&OpenIBootEnd - (uint32_t)&_start);
 	bufferPrintf("Setting NVRAM version...\r\n");
-	nvram_setvar("opib-version", "0.1");
+	nvram_setvar("opib-version", "0.1.1");
 	nvram_save(1);
 	bufferPrintf("Images installed\r\n");
 }
